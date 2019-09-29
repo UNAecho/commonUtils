8855336bd438827d503ff5210a1ae4de48cb2f75
yad <yadhr582855555@hotmail.com>
2019-09-29 14:46:06 +0800

    change impl

--- readContentOfScreen.py
+++ readContentOfScreen.py
@@ -1,5 +1,5 @@
 import pytesseract
-from PIL import Image
+from PIL import ImageGrab
 import shutil
 from datetime import datetime
 from screenshot import getScreenshot
@@ -13,25 +13,24 @@ from screenshot import getScreenshot
 # screenshot_high_y:图片y轴长度
 
 
-def read_number_of_screen(file_path, screenshot_x, screenshot_y, screenshot_wight_x, screenshot_high_y):
+def read_number_of_screen(screenshot_x_left, screenshot_y_upper, screenshot_x_right, screenshot_y_bottom):
     try:
-        getScreenshot(file_path, screenshot_x,
-                      screenshot_y, screenshot_wight_x, screenshot_high_y)
-        read_screen_text = pytesseract.image_to_string(Image.open(file_path))
+        # 截屏，图片直接加入内存中,并使用.convert('L')处理为灰度图。
+        # point规定除了255白色的像素点，都专为纯黑色0像素，而其它任何颜色，都转为255白色。
+        # 理由是想做成白纸黑字的形式，便于OCR读取。
+        # lambda是隐式函数的写法，具体网上可以搜到用法。
+        im = ImageGrab.grab((screenshot_x_left, screenshot_y_upper, screenshot_x_right, screenshot_y_bottom)).convert('L').point(lambda x:0 if x ==255 else 255)
+        read_screen_text = pytesseract.image_to_string(im)
         if read_screen_text is None or "":
-            print("带数字的图片路径：" + file_path + " 没读出来，这程序写的什么破玩意")
-        number_of_remaining = str(read_screen_text).replace(" ", "").split("/")[0]
-        if int(number_of_remaining) <= 0:
-            return number_of_remaining.replace("-", "")
-        else:
-            return number_of_remaining
+            print("没读出来，这程序写的什么破玩意")
+        return read_screen_text
     except Exception as e:
         print("读取数字出错！错误信息：" + str(e))
         # 输出读取错误的图片，方便人工debug原因
         # 请定期清理文件，不然文件夹体积会越来越大
-        error_image_filepath = 'errorImage\\' + datetime.now().strftime('%Y%m%d%H%M%S') + '.png'
-        shutil.copyfile(file_path, error_image_filepath)
-        print("读取出错的图片路径为：" + error_image_filepath+"，请定期清理，防止文件夹越来越大")
+        # error_image_filepath = 'errorImage\\' + datetime.now().strftime('%Y%m%d%H%M%S') + '.png'
+        # shutil.copyfile(file_path, error_image_filepath)
+        # print("读取出错的图片路径为：" + error_image_filepath+"，请定期清理，防止文件夹越来越大")
         return "0"
 
 
